ae0350ccfbc414253065ccc2a2e02977135b7a32
Eduar <donsecun@gmail.com>
2024-04-30 11:05:45 +0200

    another advance

--- knight.js
+++ knight.js
@@ -5,13 +5,22 @@ for (let i = 0; i < 8; i++) {
   board[i] = [];
 }
 
+const boardmoves = [];// create anther board!
+for (let i = 0; i < 10; i++) {
+  boardmoves[i] = [];
+  for (let j = 0; j < 2; j++) {
+    boardmoves[i][j] = [];
+  }
+}
+let z = 0;
+
 const addMove = (x, y, level) => {  //insert the initial position!
   if ((x >= 0) && (x <= 7) && (y >= 0) && (y <= 7) && board[x][y] == null) {
     board[x][y] = level;
   }
 }
 
-const addAllMoves = (x, y, level) => { // knight movements
+/* const addAllMoves = (x, y, level) => { // knight movements
   addMove(x + 1, y + 2, level);
   addMove(x + 2, y + 1, level);
   addMove(x + 2, y - 1, level);
@@ -21,12 +30,47 @@ const addAllMoves = (x, y, level) => { // knight movements
   addMove(x - 2, y + 1, level);
   addMove(x - 1, y + 2, level);
 }
-
+ */
+const addAllMoves = (x, y, level) => { // knight movements 
+  addMove(x + 1, y + 2, level);
+    boardmoves[z++][0] = [x, y];
+    boardmoves[z][1] = [x + 1, y + 2];
+  addMove(x + 2, y + 1, level);
+    boardmoves[z++][0] = [x, y];
+    boardmoves[z][1] = [x + 2, y + 1];
+  addMove(x + 2, y - 1, level);
+    boardmoves[z++][0] = [x, y];
+    boardmoves[z][1] = [x + 2, y - 1];
+  addMove(x + 1, y - 2, level);
+    boardmoves[z++][0] = [x, y];
+    boardmoves[z][1] = [x + 1, y - 2];  
+  addMove(x - 1, y - 2, level);
+    boardmoves[z++][0] = [x, y];
+    boardmoves[z][1] = [x - 1, y - 2];
+  addMove(x - 2, y - 1, level);
+    boardmoves[z++][0] = [x, y];
+    boardmoves[z][1] = [x - 2, y - 1];
+  addMove(x - 2, y + 1, level);
+    boardmoves[z++][0] = [x, y];
+    boardmoves[z][1] = [x - 2, y + 1];
+  addMove(x - 1, y + 2, level);
+    boardmoves[z++][0] = [x, y];
+    boardmoves[z][1] = [x - 1, y + 2];
+}
+/* 
+const paths1 = (i,j) => {
+  //for ( let m = 0; m < 8; m++){
+    boardmoves[z++][0] = [i, j];
+    boardmoves[z][1] = [i, j];
+  //}
+} */
 const addAllPossible = (level) => { // scan the board 
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       if (board[i][j] === level) {
-        addAllMoves(i, j, level + 1); // call the movements fnction 
+        //paths1(i,j);
+        //boardmoves[0][z++] = [i, j];
+        addAllMoves(i, j, level + 1); // call moves function 
       }
     }
   }
@@ -41,4 +85,7 @@ const findPath = (startX, startY, endX, endY) => {
   return board[endX][endY];
 }
 
-console.log(findPath(3, 3, 2, 1));
+console.log(findPath(3, 3, 1, 4));
+console.log(boardmoves.length);
+console.log(boardmoves);
+console.log(board);
