82291fd598581a4b2409f2fd58e82dcbd6ce270b
Eduar <donsecun@gmail.com>
2024-05-18 15:41:48 +0200

    can do click on the chessboar and save them

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ center.style.alignItems = "center";
 center.style.justifyContent = "center";
 center.style.flexDirection = "column";
 
-var ChessTable = document.createElement('table'); // Create a table element
+// Create a table element  --> chessboard --------------------------------------------------------------------
+var ChessTable = document.createElement('table');
 for (var i = 0; i < 8; i++) {
     let tr = document.createElement('tr'); // Create a row
     for (var j = 0; j < 8; j++) {
@@ -28,24 +29,26 @@ for (var i = 0; i < 8; i++) {
     }
     ChessTable.appendChild(tr);
 }
+
+// from here read clicks and save them ---> cellstore --------------------------------------------------------------
 var cellstore = [];
 var x = 0;
 function getindex (){
     var tdcolor = document.getElementsByClassName('cell');
     for (var i = 0 ; i < tdcolor.length; i++) {
-        tdcolor[i].addEventListener('click', function () { // create function calculator to recall it
+        tdcolor[i].addEventListener('click', function () { // listen click on chessboard
             this.classList.add('blue');
             this.style.transform = "scale(1.08, 1.08)";
-            var indexcell=this.innerHTML;
+            var indexcell=this.innerHTML; //this is string of 3 index
             if(x == 0){
                 cellstore[0] = indexcell[0]; 
                 cellstore[1] = indexcell[2];
                 x++;
-            }
-            else{ 
-                cellstore[2] = indexcell[0]; 
-                cellstore[3] = indexcell[2];
-                x = 0;
+            }else{
+                if(x == 1){ 
+                    cellstore[2] = indexcell[0]; 
+                    cellstore[3] = indexcell[2];
+                }else{x = 0;}
             }
             console.log("cellstore " + cellstore);
         });
